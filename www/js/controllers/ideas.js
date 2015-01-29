16e3fd069a7a917756c32e57064ce552c83e8a65
Davis Cook <daviscook447@gmail.com>
2015-01-29 14:31:02 -0500

    added a button on the ideas view that links to the categories view

--- www/js/controllers/ideas.js
+++ www/js/controllers/ideas.js
@@ -5,7 +5,10 @@ angular.module('ideas.controllers.ideas', [])
   IO.syncArray(ref, $scope, "ideas", "ideasArray"); //Binds the array at ref to $scope.ideas
   $scope.goIdea = function(idea) {
     $state.go('app.comments', {ideaID: idea.$id});
-  }
+  };
+  $scope.goToCategories = function() {
+    $state.go('app.categories');
+  };
   $scope.$on('destroy', function() {
     IO.release("ideasArray");
   });
